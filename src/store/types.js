2dc9cdf31966d1eff8b968815c135df2f3441ee7
Leticia <leticia.bora@gmail.com>
2021-02-23 15:17:17 -0300

    types

--- src/store/types.js
+++ src/store/types.js
@@ -1,1 +1,2 @@
+// Esse arquivo é só pra salvar os types em uma variável pra que, caso esse type seja alterado, fique mais fácil de visualizar onde ele foi importado.
 const SET_FORMS = 'SET_FORMS';
